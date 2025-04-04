65fceaf492f4c14d65da8469fdaeb553417f5c34
Ushan Perera <gushanperera@gmail.com>
2025-04-04 19:05:08 +1030

    LoadSampleById added

--- DontNetCore-MinimalApiSample-Full/Endpints/SampleEndpoints.cs
+++ DontNetCore-MinimalApiSample-Full/Endpints/SampleEndpoints.cs
@@ -1,28 +1,64 @@
-﻿namespace DontNetCore_MinimalApiStructure.Endpints
+﻿using DontNetCore_MinimalApiStructure.Repository;
+
+namespace DontNetCore_MinimalApiStructure.Endpints
 {
     public static class SampleEndpoints
     {
         public static void AddSampleEndpoints(this WebApplication app)
         {
-            //Get the SampleData from the DI
-            var sampleData = app.Services.GetService<Repository.SampleData>();
+            //Get all samples---------------------------
 
-            //Get all samples
-            app.MapGet("/samples", () =>
-            {
-                return Results.Ok(sampleData?.sampleData);
-            });
+            // 01. Just return 
+            //app.MapGet("/samples", (Repository.SampleData data) =>
+            //{
+            //    return Results.Ok(data?.sampleData);
+            //});
 
-            //Get sample by id
-            app.MapGet("/sample/{id}", (int id) =>
-            {
-                var sample = sampleData?.sampleData.FirstOrDefault(x => x.Id == id);
-                if (sample == null)
-                {
-                    return Results.NotFound();
-                }
-                return Results.Ok(sample);
-            });
+
+            //// 02. Get the SampleData from the DI
+            //var data = app.Services.GetService<Repository.SampleData>();
+
+            //app.MapGet("/samples", () =>
+            //{
+            //    return Results.Ok(data?.sampleData);
+            //});
+
+   //app.MapGet("/sample/{id}", (int id) =>
+            //{
+            //    var sample = sampleData?.sampleData.FirstOrDefault(x => x.Id == id);
+            //    if (sample == null)
+            //    {
+            //        return Results.NotFound();
+            //    }
+            //    return Results.Ok(sample);
+            //});
+
+
+            //With the HTTP support
+            app.MapGet("/samples", (LoadAllSample));
+
+            app.MapGet("/sample/{id}", (LoadSampleById));
+
+
+         
         }
+
+
+        private static IResult LoadAllSample(SampleData data)
+        {
+            return Results.Ok(data?.sampleData);
+        }
+
+        private static IResult LoadSampleById(SampleData data, int id)
+        {
+            var sample = data?.sampleData.FirstOrDefault(x => x.Id == id);
+            if (sample == null)
+            {
+                return Results.NotFound();
+            }
+            return Results.Ok(sample);
+        }
+
+
     }
-}
+    }
